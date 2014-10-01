74d788d6c676450059341ccd7e74f94df9d6b239
Aman13 <sandhu.amandeep.s@gmail.com>
2014-10-01 12:53:15 -0700

    String comparison not working

--- Aman-Shell.c
+++ Aman-Shell.c
@@ -2,26 +2,26 @@
 #include <stdlib.h>
 #include <string.h>
 #include <time.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 #define LINE_LENGTH 100
 
-int main()
-{
+int main() {
 	//Line to record input, Character to help break apart input, Array to hold the individual characters.
-	char line[LINE_LENGTH], *character, *array[LINE_LENGTH];;
+	char line[LINE_LENGTH], *character, *array[LINE_LENGTH];
 	//Counter to record the length of the array, i is used in the for loop
 	int counter, i;
 
-	//Display the time
-	time_t now;
-	struct tm *currentTime;
-	now = time(NULL);
-	currentTime = localtime(&now);
-
 	while(1) {
+		//Display the time
+		time_t now;
+		struct tm *currentTime;
+		now = time(NULL);
+		currentTime = localtime(&now);
+
 		printf("aman-shell %d:%d:%d# ", currentTime->tm_hour, currentTime->tm_min, currentTime->tm_sec);
 		fgets(line, LINE_LENGTH, stdin);
-
 		counter = 0; //Record size of array
 		character = strtok(line, " ");
 
@@ -32,9 +32,16 @@ int main()
 		}
 		//Print whats in array
 		for(i = 0; i < counter; i++) {
-			printf("%s", array[i]);
+			printf("array position %d contains: %s\n",i, array[i]);
 		}
+		printf("searching for command:%s\n", array[0]);
+		if(strcmp("exit", array[0]) == 0) {
+			printf("Exiting aman-shell");
+			return EXIT_SUCCESS;
+		}else if(strcmp("ls", array[0]) == 0) { 
 
+		}else {
+			printf("Command not recognized \n");
+		}
 	}
-	return EXIT_SUCCESS;
 }
