0ac05d3c22f5b23c5edf0556472ab5fcc7200a71
Aman13 <sandhu.amandeep.s@gmail.com>
2014-10-02 18:15:40 -0700

    Need to remove \n from last entry of tokenized string

--- Aman-Shell.c
+++ Aman-Shell.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <time.h>
 #include <unistd.h>
+#include <sys/wait.h>
+
 
 #define LINE_LENGTH 100
 
@@ -11,7 +13,9 @@ int main() {
 	//Line to record input, Character to help break apart input, Array to hold the individual characters.
 	char line[LINE_LENGTH], *character, *array[LINE_LENGTH];
 	//Counter to record the length of the array, i is used in the for loop
-	int counter, i;
+	int counter, i, status, exitid;
+	//Help keep track of child process
+	pid_t pcopy;
 
 	while(1) {
 		//Display the time
@@ -31,27 +35,32 @@ int main() {
 			character = strtok(NULL, " ");
 		}
 		//Print whats in array
-//		for(i = 0; i < counter; i++) {
-//			printf("array position %d contains: %s\n",i, array[i]);
-//		}
+		for(i = 0; i < counter; i++) {
+			printf("array position %d contains: %s\n",i, array[i]);
+		}
 		printf("searching for command:%s\n", array[0]);
 		if((strcmp("exit\n", array[0]) == 0) || strcmp("exit()\n", array[0]) == 0){
 			printf("Exiting aman-shell\n");
 			return EXIT_SUCCESS;
 		}else if(strcmp("echo", array[0]) == 0) {
 			for(i = 1; i < counter; i++) {
-				printf("%s",array[i]);
-			}			
+				printf("%s ",array[i]);
+			}
 		}else {
-			pid_t child;
-			child = fork();
+			pcopy = fork();
 			//fork succeeded
-			if(child >= 0) {
-				printf("Child PID: %d Parent PID: %d\n", getpid(), getppid());
+			if(pcopy >= 0) {
+				if(pcopy == 0) {
+					printf("copy pid:%d\nparent pid:%d\nvalue of copy:%d\n", getpid(), getppid(), pcopy);
+					exitid = execvp(array[0], array);
+					return exitid;
+				}else {
+					waitpid(pcopy, &status, WUNTRACED);
+					printf("copy process has terminated\n");
+				}
 
 			}else { //Fork failed
 				printf("Failed to create fork");
-				exit(0);
 			}
 		}
 	}
