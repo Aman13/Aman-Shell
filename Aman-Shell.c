b13f49818da19fe8908a008e33c791b067875c78
Aman13 <sandhu.amandeep.s@gmail.com>
2014-10-02 20:35:06 -0700

    Added comments

--- Aman-Shell.c
+++ Aman-Shell.c
@@ -1,3 +1,4 @@
+//Amandeep Sandhu, student id: 301073950, Oct 2, 2014
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -5,12 +6,12 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
-
+//maximum length for command
 #define LINE_LENGTH 100
 
 int main() {
 
-	//Line to record input, Character to help break apart input, Array to hold the individual characters.
+	//Line to record input, Character to help break apart input, Array to hold tokenized string.
 	char line[LINE_LENGTH], *character, *array[LINE_LENGTH];
 	//Counter to record the length of the array, i is used in the for loop
 	int counter, i, status, exitid;
@@ -28,39 +29,38 @@ int main() {
 		now = time(NULL);
 		currentTime = localtime(&now);
 
-		printf("aman-shell:%d:%d:%d#", currentTime->tm_hour, currentTime->tm_min, currentTime->tm_sec);
+		printf("%d:%d:%d#", currentTime->tm_hour, currentTime->tm_min, currentTime->tm_sec);
 		fgets(line, LINE_LENGTH, stdin);
-		counter = 0; //Record size of array
+		//record amount of tokens in array
+		counter = 0;
+		//remove spaces and \n from tokens
 		character = strtok(line, " \n");
-
 		//Break string into tokens
 		while(character != NULL) {
 			array[counter++] = character;
 			character = strtok(NULL, " \n");
 		}
-
+		//exit command for shell
 		if((strcmp("exit", array[0]) == 0) || strcmp("exit()", array[0]) == 0){
 			printf("Exiting aman-shell\n");
 			return EXIT_SUCCESS;
-//		}else if(strcmp("echo", array[0]) == 0) {
-//			for(i = 1; i < counter; i++) {
-//				printf("%s ",array[i]);
-//			}
-//			printf("\n");
 		}else {
 			pcopy = fork();
 			//fork succeeded
 			if(pcopy >= 0) {
+				//Child or copy loop, to execute program
 				if(pcopy == 0) {
-			//		printf("copy pid:%d\nparent pid:%d\nvalue of copy:%d\n", getpid(), getppid(), pcopy);
 					exitid = execvp(array[0], array);
+					if(exitid < 0) {
+						printf("Failed to find command\n");
+					}
 					return exitid;
 				}else {
+					//parent waits for child to execute or fail
 					waitpid(pcopy, &status, WUNTRACED);
-					printf("copy process has terminated\n");
 				}
-
-			}else { //Fork failed
+			//Fork failed	
+			}else { 
 				printf("Failed to create fork");
 			}
 		}
